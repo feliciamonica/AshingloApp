81e991a6c1c4a3d1ac4ac5551079e354b9c44e6d
kerry-kate <kerry.kemajou@2027.ucac-icam.com>
2025-06-20 13:27:19 +0200

    Ajout du projet frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageConnexion from './ashinglo/login';  
+import Acceuil from './ashinglo/acceuil';
+
+function App() {
+  return (
+    <div className="App">
+      <PageConnexion />
+      <Acceuil />
+    </div>
+  );
+}
+
+export default App;
